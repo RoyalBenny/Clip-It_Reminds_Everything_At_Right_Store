f87294cccdc62226a9138da4b252abad3fbc4de0
Royal Benny <royalbenny2@gmail.com>
2019-12-12 13:12:33 +0530

    updated

--- app/src/main/java/com/clipit/cliptit2/createDirectory/CategoryClassAdapter.kt
+++ app/src/main/java/com/clipit/cliptit2/createDirectory/CategoryClassAdapter.kt
@@ -77,7 +77,76 @@ class CategoryClassAdapter(val list:ArrayList<ShopClass>, var placeAutocompleteA
 
                 popupMenu.setOnMenuItemClickListener { pi->
                     when(pi.itemId){
-
+                        R.id.atm_menu ->{
+                            itemView.catergory_chip_edit.chipText = "ATM"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "atm"
+                        }
+                        R.id.bank_menu ->{
+                            itemView.catergory_chip_edit.chipText = "BANK"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "bank"
+                        }
+                        R.id.bar_menu ->{
+                            itemView.catergory_chip_edit.chipText = "BAR"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "bar"
+                        }
+                        R.id.bus_station_menu ->{
+                            itemView.catergory_chip_edit.chipText = "BUS STATION"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "bus_station"
+                        }
+                        R.id.cafe_menu ->{
+                            itemView.catergory_chip_edit.chipText = "BAKERY"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "bakery"
+                        }
+                        R.id.car_wash_menu ->{
+                            itemView.catergory_chip_edit.chipText = "CAR WASH"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "car_wash"
+                        }
+                        R.id.gas_station_menu ->{
+                            itemView.catergory_chip_edit.chipText = "GAS STATION"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "gas_station"
+                        }
+                        R.id.hospital_menu ->{
+                            itemView.catergory_chip_edit.chipText = "HOSPITAL"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "hospital"
+                        }
+                        R.id.pharmacy_menu ->{
+                            itemView.catergory_chip_edit.chipText = "PHARMACY"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "pharmacy"
+                        }
+                        R.id.restaurant ->{
+                            itemView.catergory_chip_edit.chipText = "RESTAURANT"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "restaurant"
+                        }
+                        R.id.school_menu ->{
+                            itemView.catergory_chip_edit.chipText = "SCHOOL"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "school"
+                        }
+                        R.id.shop_menu ->{
+                            itemView.catergory_chip_edit.chipText = "STORE"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "store"
+                        }
+                        R.id.taxi_menu ->{
+                            itemView.catergory_chip_edit.chipText = "TAXI STATION"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "taxi_station"
+                        }
+                        R.id.train_menu ->{
+                            itemView.catergory_chip_edit.chipText = "TRAIN STATION"
+                            itemView.catergory_chip_edit.chipIcon =  pi.icon
+                            GlobalCategory.returnGlobalCategory()[layoutPosition].shopCategory = "train_station"
+                        }
 
                     }
 
